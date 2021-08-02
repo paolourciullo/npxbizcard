38f2539ac18a60db6a22e523a9fb4732e1e354c9
Paolo U <paolourciullo@gmail.com>
2021-08-02 17:18:25 -0400

    add emojis

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 console.log(`
   Paolo Urciullo
   Site - www.paolourciullo.com 🌍
-  Twitter - www.twitter.com/paolo__init__
-  Github - www.github.com/paolourciullo  
+  Twitter - www.twitter.com/paolo__init__ 🦜
+  Github - www.github.com/paolourciullo  🏢
 `)
